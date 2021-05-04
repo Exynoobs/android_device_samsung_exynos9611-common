0cb8856221d05544a1dd09ed3e44b7e3c755e823
Jan Altensen <info@stricted.net>
2021-05-04 02:43:07 +0200

    exynos9611-common: build common usb typec hal
    Change-Id: I3a12e8d96ee4bb347426aaad12545a0afd925779

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
+### USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.1-service.typec
+
 ### USB TRUST HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
